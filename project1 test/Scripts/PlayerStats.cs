d43387a4de6348284957b5517182d3086edc8e92
NourrAhmed <nourahmed.2164@gmail.com>
2024-12-08 21:39:14 +0200

    PlayerStats scripts

---
+++ project1 test/Scripts/PlayerStats.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerStats : MonoBehaviour
+{
+
+    public int health = 6;
+    public int lives = 3;
+    private float flickerTime = 0f;
+    public float flickerDuration = 0.1f;
+    private SpriteRenderer spriteRenderer;
+    public bool isImmune = false;
+    private float immunityTime = 0f;
+    public float immunityDuration = 1.5f;
+    public int coinsCollected = 0;
+
+    public AudioClip GameOverSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isImmune == true)
+        {
+            SpriteFlicker();
+            immunityTime = immunityTime + Time.deltaTime;
+            if (immunityTime >= immunityDuration)
+            {
+                this.isImmune = false;
+                this.spriteRenderer.enabled = true;
+            }
+        }
+    }
+    void SpriteFlicker()
+    {
+        if (this.flickerTime < this.flickerDuration)
+        {
+            this.flickerTime = this.flickerTime + Time.deltaTime; ;
+        }
+        else if (this.flickerTime >= this.flickerDuration)
+        {
+            spriteRenderer.enabled = !(spriteRenderer.enabled);
+            this.flickerTime = 0;
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        if (this.isImmune == false)
+        {
+            this.health = this.health - damage;
+            if (this.health < 0)
+            {
+                this.health = 0;
+            }
+            if (this.lives > 0 && this.health == 0)
+            {
+                FindObjectOfType<LevelManager>().RespawnPlayer();
+                this.health = 6;
+                this.lives--;
+            }
+            else if (this.lives == 0 && this.health == 0)
+            {
+                (new NavigationController()).GoToGameOverScene();
+                Debug.Log("GameOver");
+                AudioController.instance.PlaySingle(GameOverSound);
+                AudioController.instance.musicSource.Stop();
+                Destroy(this.gameObject);
+            }
+            Debug.Log("Player Health:" + this.health.ToString());
+            Debug.Log("Player Lives:" + this.lives.ToString());
+
+        }
+        PlayHitReaction();
+    }
+    void PlayHitReaction()
+    {
+        this.isImmune = true;
+        this.immunityTime = 0f;
+    }
+
+}
