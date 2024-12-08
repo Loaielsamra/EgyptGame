e66c5afdb48196184708bfc0c9708b778dd8c188
dimakotb <dimasobhy12@gmail.com>
2024-12-08 21:52:23 +0200

    push EnemyController script
    write script

---
+++ project1 test/Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EnemyController : MonoBehaviour
+{
+
+    public bool isFacingRight = false;
+    public float maxSpeed = 3f;
+    public int damage = 6;
+    public AudioClip hit1;
+    public AudioClip hit2;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.localScale = new Vector3(-(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            AudioController.instance.RandomizeSfx(hit2, hit1);
+            FindObjectOfType<PlayerStats>().TakeDamage(damage);
+        }
+    }
+
+}
